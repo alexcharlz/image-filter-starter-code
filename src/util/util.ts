f256ccf91361c30f5f8e7dc25acde03166857678
Idenyi Gabriel <idenyigabriel@gmail.com>
2023-01-15 16:28:30 +0100

    - install axios to fix issue with jimp unable to read images with redirect

--- src/util/util.ts
+++ src/util/util.ts
@@ -2,6 +2,7 @@ const URL = require('url').URL
 
 import fs from "fs";
 import Jimp = require("jimp");
+import axios from 'axios'
 
 // filterImageFromURL
 // helper function to download, filter, and save the filtered image locally
@@ -13,7 +14,16 @@ import Jimp = require("jimp");
 export async function filterImageFromURL(inputURL: string): Promise<string> {
   return new Promise(async (resolve, reject) => {
     try {
-      const photo = await Jimp.read(inputURL);
+      const photo = await axios({
+        method: 'get',
+        url: inputURL,
+        responseType: 'arraybuffer'
+      })
+      .then(function ({data: imageBuffer}) {
+        return Jimp.read(imageBuffer)
+      })
+      // const photo = await Jimp.read("https://images.pexels.com/photos/296282/pexels-photo-296282.jpeg?auto=compress&cs=tinysrgb&h=350")
+      // .then(image => image.getBuffer(Jimp.AUTO, (err, res) => res));
       const outpath =
         "/tmp/filtered." + Math.floor(Math.random() * 2000) + ".jpg";
       await photo
